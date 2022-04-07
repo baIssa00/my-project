d6b9d12a35cd85ceae92e3379961498dca91d0d6
baIssa00 <issaba.00@gmail.com>
2022-04-07 18:33:06 +0200

    second commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/BlogController.php',
+        ]);
+    }
+    
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('base.html.twig');
+    }
+}
